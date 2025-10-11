2daa0475c7f49fefa3e74e20d2d85c7235c40b45
Alan Hsieh <twcch1218@gmail.com>
2025-10-11 18:02:42 +0800

    Add Product model and register it in admin

--- server/productapp/models.py
+++ server/productapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    image_url = models.URLField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product_name
