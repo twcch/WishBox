34846eabc0591d7da80453eb6691314b83856cac
Alan Hsieh <twcch1218@gmail.com>
2025-11-15 09:58:53 +0800

    Refactor admin.py to improve comment clarity for interface adjustments

--- server/userapp/admin.py
+++ server/userapp/admin.py
@@ -4,7 +4,7 @@ from .models import CustomerUser
 # Register your models here.
 admin.site.register(CustomerUser)
 
-# 自訂後台標題
+# 調整介面
 admin.site.site_header = "WishBox 管理後台"  # 左上角標題
 admin.site.site_title = "WishBox 後台"  # 瀏覽器標籤頁標題
 admin.site.index_title = "歡迎使用 WishBox 管理系統"  # 首頁標題
